b1ec236c5799eefaf27fd76a19326a35b49b36f5
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-09-18 13:43:00 +0600

    Recapping Var & Scope

---
+++ JavaScript Pro - Mastering Advanced Concepts & Techniques/09-The-Tricky-Parts-Scope-&-Closures/script.js
@@ -0,0 +1,89 @@
+'use strict';
+
+//* Recapping Var & Scope
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Recapping Let, Const, & Scope
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* The Scope Chain
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Static Scope
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Hoisting
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* IIFEs
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Closures: The Basics
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Closures: Another Example
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Closures: Factory Functions
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Closures: Event Listeners
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
+
+//* Closures: Loops
+
+// ChatGPT 👇
+
+{
+  // TODO: ✅ Read ChatGPT's Article.
+}
