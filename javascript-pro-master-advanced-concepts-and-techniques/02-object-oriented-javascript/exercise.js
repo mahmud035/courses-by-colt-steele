4e1a707de354fef202c921c8f725f74833d91dcb
mahmud035 <mahamudulhasan4148@gmail.com>
2025-09-28 12:10:44 +0600

    section 02: oop in js complete

--- javascript-pro-master-advanced-concepts-and-techniques/02-object-oriented-javascript/exercise.js
+++ javascript-pro-master-advanced-concepts-and-techniques/02-object-oriented-javascript/exercise.js
@@ -1,1 +1,85 @@
 'use strict';
+
+//* Ebook Exercise
+
+// Instructions
+
+/* 
+1. Create a class named `Book` with a constructor that initializes a `title`, `author`, and `year` properties. The `title` and `author` should be strings and the `year` should be a number
+
+2. Create a subclass named `Ebook` that extends Book.
+
+3. The `Ebook` class should have an additional property `fileSize` which is a number and a method `download()` that returns a string with the `title` of the ebook and its `fileSize`. 
+*/
+
+/* 
+{
+  class Book {
+    constructor(title, author, year) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+    }
+  }
+
+  class Ebook extends Book {
+    constructor(title, author, year, fileSize) {
+      super(title, author, year);
+      this.fileSize = fileSize;
+    }
+
+    download() {
+      return `Downloading ${this.title}, size ${this.fileSize} MB...`;
+    }
+  }
+
+  const ebook = new Ebook('Eloquent JavaScript', 'Marijn Haverbeke', 2024, 10);
+
+  ebook.download(); // Downloading Eloquent JavaScript, size 10 MB...
+  console.log(ebook);
+
+  console.log(ebook instanceof Ebook); // true
+  console.log(ebook instanceof Book); // true
+
+  console.log(ebook.__proto__ === Ebook.prototype); // true
+  console.log(ebook.__proto__.__proto__ === Book.prototype); // true
+  console.log(ebook.__proto__.__proto__.__proto__ === Object.prototype); // true (Top of the prototype chain)
+  console.log(ebook.__proto__.__proto__.__proto__.__proto__); // null
+}
+ */
+
+//* ArrayUtils exercise
+
+// Instructions
+
+/*
+Implement a class named `ArrayUtils` that cannot be instantiated and contains static methods `average` and `max`. If the class is instantiated throw an error with the message 'ArrayUtils cannot be instantiated.'
+
+The `average` method should return the average of an array of numbers. If the array is empty, throw an error with the message 'Array cannot be empty.'
+
+The `max` method should return the largest number from an array of numbers. You can assume you will always get passed an array of numbers
+*/
+
+/* 
+{
+  class ArrayUtils {
+    constructor() {
+      throw new Error('ArrayUtils cannot be instantiated');
+    }
+
+    static average(arr) {
+      if (arr.length === 0) throw new Error('Array cannot be empty');
+
+      const sum = arr.reduce((sum, curr) => sum + curr, 0);
+      return sum / arr.length;
+    }
+
+    static max(arr) {
+      return Math.max(...arr);
+    }
+  }
+
+  ArrayUtils.average([1, 2, 3]); // 2
+  ArrayUtils.max([1, 2, 3]); // 3
+}
+ */
--- javascript-pro-master-advanced-concepts-and-techniques/02-object-oriented-javascript/lectures.js
+++ javascript-pro-master-advanced-concepts-and-techniques/02-object-oriented-javascript/lectures.js
@@ -46,6 +46,7 @@
 
 //* Mixing Data & Functions With Objects
 
+/* 
 {
   // Imagine some useful functions:
   function getTriangleArea(a, b) {
@@ -86,43 +87,268 @@
   // - Difficult to maintain
   // - If we have 1000 triangles, we’d have 1000 copies of these functions — that’s going to waste memory!
 }
+ */
 
 //* Class Basics
-
-{
-}
-
 //* Constructor Function
-
-{
-}
-
 //* Instance Methods / Public Methods
 
+/* 
 {
+  class Triangle {
+    constructor(a, b) {
+      // Validate data
+      if (!Number.isFinite(a) || a <= 0) throw new Error(`Invalid a: ${a}`);
+      if (!Number.isFinite(b) || b <= 0) throw new Error(`Invalid b: ${b}`);
+
+      // Instance properties
+      this.a = a;
+      this.b = b;
+    }
+
+    // Public methods
+    getArea() {
+      return (this.a * this.b) / 2;
+    }
+
+    getHypotenuse() {
+      return Math.sqrt(this.a ** 2 + this.b ** 2);
+    }
+
+    describe() {
+      return `Area is ${this.getArea()}`;
+    }
+  }
+
+  // Create instance using class
+  const triangle = new Triangle(3, 4); // <- calls constructor
+  triangle.getArea(); // 6
+  triangle.getHypotenuse(); // 5
+  triangle;
+
+  typeof triangle; // object
+  triangle instanceof Triangle; // true
 }
+ */
+
+//* Practice Time Bank Account
+
+/* 
+{
+  // Create a `BankAccount` class
+  //  - Properties
+  //      - balance (defaults to 0 if not provided)
+  //      - accountHolder
+  //      - accountNumber
+  // - Methods
+  //   - deposit(amount) - increases balance by amount
+  //   - withdraw(amount) - decreases balance by amount
+
+  class BankAccount {
+    constructor(balance = 0, accountHolder, accountNumber) {
+      this.balance = balance;
+      this.accountHolder = accountHolder;
+      this.accountNumber = accountNumber;
+    }
+
+    deposit(amount) {
+      this.balance += amount;
+    }
+
+    withdraw(amount) {
+      this.balance -= amount;
+    }
+
+    getBalance() {
+      console.log(this.balance);
+    }
+  }
+
+  const jonasAccount = new BankAccount(10000, 'Jonas', '*****789');
+  jonasAccount.deposit(5000);
+  jonasAccount.getBalance(); // 15000
+  jonasAccount.withdraw(3000);
+  jonasAccount.getBalance(); // 12000
+}
+ */
 
 //* Inheritance Basics
+//* The `super` Keyword
 
+/* 
 {
-}
+  //* Parent Class
+  class Triangle {
+    constructor(a, b) {
+      this.a = a;
+      this.b = b;
+    }
 
-//* The Super Keyword
+    // Public methods
+    getArea() {
+      return (this.a * this.b) / 2;
+    }
 
-{
+    getHypotenuse() {
+      return Math.sqrt(this.a ** 2 + this.b ** 2);
+    }
+
+    describe() {
+      return `Area is ${this.getArea()}`;
+    }
+  }
+
+  //* Child Class
+  class ShyTriangle extends Triangle {
+    constructor(a, b) {
+      // call parent constructor
+      super(a, b);
+    }
+
+    // inherits `getArea`, `getHypotenuse`
+
+    // "override" describe() with/new version
+    describe() {
+      return `runs and hides`;
+    }
+  }
+
+  // Create instance using ShyTriangle class
+  const shy = new ShyTriangle(3, 4);
+  console.log(shy.getArea()); // 6
+
+  console.log(shy instanceof ShyTriangle); // true
+  console.log(shy instanceof Triangle); // true
+
+  //* Another Child Class: `ColorTriangle`
+  class ColorTriangle extends Triangle {
+    constructor(a, b, color) {
+      // call parent constructor
+      super(a, b);
+      this.color = color;
+    }
+
+    // inherits `getArea`, `getHypotenuse`
+
+    // "override" describe() with/new version
+    describe() {
+      return `Area is ${this.getArea()}.` + ` Color is ${this.color}!`;
+    }
+  }
+
+  //* Multi-level inheritance
+  // it’s even possible to subclass a subclass, making a kind of hierarchy. Here’s a special kind of ColorTriangle:
+
+  class RainbowTriangle extends ColorTriangle {
+    constructor(a, b, color) {
+      // call parent constructor
+      super(a, b, color);
+    }
+
+    // inherits `getArea`, `getHypotenuse`
+
+    describe() {
+      return "I'm a beautiful rainbow!";
+    }
+  }
+
+  // Create instance using RainbowTriangle class
+  const rainbowTri = new RainbowTriangle(3, 4, 'rainbow');
+  console.log(rainbowTri);
+
+  console.log(rainbowTri instanceof RainbowTriangle); // true
+  console.log(rainbowTri instanceof ColorTriangle); // true
+  console.log(rainbowTri instanceof Triangle); // true
+
+  console.log(rainbowTri.__proto__ === RainbowTriangle.prototype); // true
+  console.log(rainbowTri.__proto__.__proto__ === ColorTriangle.prototype); // true
+  console.log(rainbowTri.__proto__.__proto__.__proto__ === Triangle.prototype); // true
+  console.log(
+    rainbowTri.__proto__.__proto__.__proto__.__proto__ === Object.prototype
+  ); // true (Top of the prototype chain)
+  console.log(rainbowTri.__proto__.__proto__.__proto__.__proto__.__proto__); // null
 }
+ */
 
 //* Static Properties
-
-{
-}
-
 //* Static Methods
 
+/* 
 {
+  // JS gives us “static methods”, where the method is called on a Class, not an object.
+
+  class CatWithStaticMethod {
+    constructor(name) {
+      this.name = name;
+    }
+
+    static myStaticMethod() {
+      console.log('myStaticMethod this = ', this);
+    }
+
+    myMethod() {
+      console.log('myMethod this = ', this);
+    }
+  }
+
+  const fluffyWithStatic = new CatWithStaticMethod('Fluffy');
+  fluffyWithStatic.myMethod(); // logs the instance
+
+  CatWithStaticMethod.myStaticMethod(); // logs the class
+
+  // Modern JS also offers “static properties”, where individual pieces of data are on the class, not on instances.
+
+  class CatWithStaticProp {
+    constructor(name) {
+      this.name = name;
+    }
+
+    // good example of a static property --
+    // all instances of cats are the same species;
+    // it doesn't vary from one cat to another
+    static genusSpecies = 'feline catus';
+
+    describe() {
+      return `${this.name} is a ${CatWithStaticProp.genusSpecies}`;
+    }
+  }
+
+  console.log(CatWithStaticProp.genusSpecies); // feline catus
 }
+ */
 
 //* Use Cases For Static Methods
 
+/* 
 {
+  // IMPORTANT: Use Case For Static Methods:
+  // - Group similar functionality together
+  // - Use as factory function that generate new instances
+
+  class Cat {
+    constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+
+    // Here, `registerStray` is a factory function/method
+    static registerStray() {
+      const names = [
+        'Muffin',
+        'Biscuit',
+        'Sleepy',
+        'Dodo',
+        'Princess Butterface',
+      ];
+
+      const name = choice(names);
+      return new Cat(name, 'unknown'); // crate new cat instance
+    }
+  }
+
+  function choice(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
 }
+ */
